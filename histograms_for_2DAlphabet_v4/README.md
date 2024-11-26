b31d5bd436575e2b26be465941fd098cdb91ee45
SanjitMasanam <sanjitmasanam@gmail.com>
2024-11-25 17:36:06 -0800

    Reorganized repository for both VR/SR. Updated README to improve clarity

--- histograms_for_2DAlphabet_v4/README.md
+++
@@ -1,3 +0,0 @@
-# histograms_for_2DAlphabet_v4
-
-### These files correspond to Cosmics Run 3 in the Validation Region (RNN [0.1,0.2])
--- runWith1DVanilla_v1_SR.py
+++ runWith1DVanilla_v1_SR.py
@@ -169,8 +169,8 @@ def plot_fit(signal, tf):
     print("Doing twoD.ledger.select")
     subset = twoD.ledger.select(_select_signal, '{}'.format(signal), tf) 
     print("Doing twoD.StdPlots")
-    twoD.StdPlots('{}-{}_area'.format(signal, tf), subset, lumiText=r'2023Dv1 (1.1M Events)', pf_slice_str={"fail":"RNNScore < 0.2","pass":"0.2 <= RNNScore"})
-    twoD.StdPlots('{}-{}_area'.format(signal, tf), subset, True, lumiText=r'2023Dv1 (1.1M Events)', pf_slice_str={"fail":"RNNScore < 0.2","pass":"0.2 <= RNNScore"})
+    twoD.StdPlots('{}-{}_area'.format(signal, tf), subset, lumiText=r'2023Dv1 (1.1M Events)', pf_slice_str={"fail":"RNNScore < 0.2","pass":"0.2 < RNNScore"})
+    twoD.StdPlots('{}-{}_area'.format(signal, tf), subset, True, lumiText=r'2023Dv1 (1.1M Events)', pf_slice_str={"fail":"RNNScore < 0.2","pass":"0.2 < RNNScore"})
 
 def GOF(signal,tf,condor=True, extra=''):
     # replace the blindedFit option in the config file with COMMENT to effectively "unblind" the GoF
