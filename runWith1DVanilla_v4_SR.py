3a44d7b6001facd3484b8133f1d548c2c7c3a249
SanjitMasanam <sanjitmasanam@gmail.com>
2025-05-12 00:05:53 -0700

    Updated v4 SR results and 1D limit

--- runWith1DVanilla_v4_SR.py
+++ runWith1DVanilla_v4_SR.py
@@ -364,12 +364,12 @@ def test_FTest(poly1, poly2, signal=''):
 if __name__ == "__main__":
     make_workspace()
 
-    signal_areas = ["Signal_M500GeV","Signal_M1000GeV","Signal_M1500GeV","Signal_M1500GeV","Signal_M1500GeV","Signal_M1500GeV","Signal_M2000GeV","Signal_M2500GeV","Signal_M3000GeV","Signal_M3500GeV","Signal_M4000GeV","Signal_M4500GeV","Signal_M5000GeV"]
-    tf_types = ['2x0','2x0','0x0','1x0','expo','2x0','2x0','2x0','2x0','2x0','2x0','2x0','2x0']
+    signal_areas = ["Signal_M500GeV","Signal_M1000GeV","Signal_M1500GeV","Signal_M2000GeV","Signal_M2500GeV","Signal_M3000GeV","Signal_M3500GeV","Signal_M4000GeV","Signal_M4500GeV","Signal_M5000GeV"]
+    tf_types = ['2x0','2x0','2x0','2x0','2x0','2x0','2x0','2x0','2x0','2x0']
 
     for signal, tf_type in zip(signal_areas,tf_types) :
       # IGNORE: When there are 100 signals, let's make sure we only run on the ones we didnt do before
-      if os.path.exists(workingArea + "/" + signal + f"-{tf_type}_area/done") : continue
+      # if os.path.exists(workingArea + "/" + signal + f"-{tf_type}_area/done") : continue
       fitPassed = False
       # If the fit failed iterate on rMax
       rMax = 50
@@ -383,14 +383,14 @@ if __name__ == "__main__":
           rMax = rMax / 2.
       plot_fit(signal,tf_type)
       print("\n\n\nFit is succesful, running limits now for " + str(signal))
-      #run_limits(signal,tf_type)
+      run_limits(signal,tf_type)
       #GOF(signal,tf_type,condor=False)
       #plot_GOF(signal,tf_type,condor=False)
       #if signal == "Signal_M500GeV":
       #  for r in [0,0.1,0.5,1,2,3]:
       #      SignalInjection(signal, tf_type, r=r, condor=False)
       #      plot_SignalInjection(signal, tf_type, r=r, condor=False)
-      Impacts(signal,tf_type,toys=500)
+      #Impacts(signal,tf_type,toys=500)
       os.system("cp " + workingArea + "/base.root " + workingArea + "/" + signal + f"-{tf_type}_area/.")
       open(workingArea + "/" + signal + f"-{tf_type}_area/done", 'w').close()
     #test_FTest('1x0','2x0',"Signal_M500GeV")
