10c2bb6efd052cb8fc26c63fdcbb0f28b2263499
SanjitMasanam <sanjitmasanam@gmail.com>
2025-05-23 00:40:34 -0700

    Added 2x0 SR results for v5 binning and exp lim plot

--- runWith1DVanilla_v4_SR.py
+++ runWith1DVanilla_v4_SR.py
@@ -369,8 +369,8 @@ def test_FTest(poly1, poly2, signal=''):
 if __name__ == "__main__":
     make_workspace()
 
-    signal_areas = ["Signal_M3500GeV"]#,"Signal_M1000GeV","Signal_M1500GeV","Signal_M2000GeV","Signal_M2500GeV","Signal_M3000GeV","Signal_M3500GeV","Signal_M4000GeV","Signal_M4500GeV","Signal_M5000GeV"]
-    tf_types = ['2x0']#'1x0','1x0','1x0','1x0','1x0','1x0','1x0','1x0','1x0']
+    signal_areas = ["Signal_M500GeV","Signal_M1000GeV","Signal_M1500GeV","Signal_M2000GeV","Signal_M2500GeV","Signal_M3000GeV","Signal_M3500GeV","Signal_M4000GeV","Signal_M4500GeV","Signal_M5000GeV"]
+    tf_types = ['2x0','2x0','2x0','2x0','2x0','2x0','2x0','2x0','2x0','2x0']
 
     for signal, tf_type in zip(signal_areas,tf_types) :
       # IGNORE: When there are 100 signals, let's make sure we only run on the ones we didnt do before
