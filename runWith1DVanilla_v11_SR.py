09bad79055e74ca3150574cf88d91f0b10b14257
SanjitMasanam <sanjitmasanam@gmail.com>
2025-09-19 19:23:17 -0700

    Updated 2D limit results

--- runWith1DVanilla_v11_SR.py
+++ runWith1DVanilla_v11_SR.py
@@ -370,7 +370,7 @@ def test_FTest(poly1, poly2, signal=''):
 if __name__ == "__main__":
     make_workspace()
     
-    signal_areas = ["Signal_M800GeV"]#"Signal_M900GeV","Signal_M1000GeV","Signal_M1100GeV","Signal_M1200GeV","Signal_M1300GeV","Signal_M1400GeV",]
+    signal_areas = ["Signal_M1800GeV"]#"Signal_M900GeV","Signal_M1000GeV","Signal_M1100GeV","Signal_M1200GeV","Signal_M1300GeV","Signal_M1400GeV",]
     #signal_areas = ["Signal_M1500GeV","Signal_M1600GeV","Signal_M1700GeV","Signal_M1800GeV","Signal_M1900GeV","Signal_M2000GeV"]#,"Signal_M2100GeV","Signal_M2200GeV","Signal_M2300GeV","Signal_M2400GeV","Signal_M2500GeV","Signal_M2600GeV","Signal_M2700GeV","Signal_M2800GeV","Signal_M2900GeV","Signal_M3000GeV","Signal_M3250GeV","Signal_M3500GeV","Signal_M3750GeV","Signal_M4000GeV","Signal_M4250GeV","Signal_M4500GeV","Signal_M4750GeV","Signal_M5000GeV"]
     tf_types = ['2x0'] * 1
 
